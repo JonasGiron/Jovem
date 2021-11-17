24e33bfab2d6735d9701bdfa6aab10f80737b3a9
Vinicius Gustavo Rodrigues de Abreu <abreuvini-2020@hotmail.com>
2021-11-17 20:23:20 -0300

    Tirando lixo

--- dezmilpastas.py
+++ dezmilpastas.py
@@ -1,7 +1,13 @@
 import pathlib
+import os
 
 
-for i in range(10000):
-    pathlib.Path(f'{i}/').mkdir()
-    with open(f'./{i}/helloworld.txt', 'w') as helloworld:
-        helloworld.write(f'OLÁ MUNDO {i}')
+for i in pathlib.Path('./').iterdir():
+    if i.name in [str(i) for i in range(10000)]:
+        os.system(f'rmdir /S /Q {i}')
+
+
+# for i in range(10000):
+#     pathlib.Path(f'{i}/').mkdir()
+#     with open(f'./{i}/helloworld.txt', 'w') as helloworld:
+#         helloworld.write(f'OLÁ MUNDO {i}')
