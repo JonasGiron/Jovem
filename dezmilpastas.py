74fb3a0052767da4b42a66dd68d954c3762df93b
julianoCpadilha <jpadilha13@gmail.com>
2021-11-17 20:01:18 -0300

    TESTE
    TERESE

--- dezmilpastas.py
+++
@@ -1,7 +0,0 @@
-import pathlib
-
-
-for i in range(10000):
-    pathlib.Path(f'{i}/').mkdir()
-    with open(f'./{i}/helloworld.txt', 'w') as helloworld:
-        helloworld.write(f'OLÁ MUNDO {i}')
--- main.py
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-print('Olá colegas!')
-=======
-print ("Olá colegas!")
->>>>>>> 5457f0e278467f85a399313904bc4e77fc158ba7
